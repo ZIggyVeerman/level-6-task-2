aee808da7614b1185f05a7d0bba630ad78feb5bf
Ziggy Veerman <ziggy.veerman@hva.nl>
2020-05-16 22:22:46 +0200

    added class WIP

--- app/src/main/java/com/example/popularmovieskotlin/MovieApi.kt
+++ app/src/main/java/com/example/popularmovieskotlin/MovieApi.kt
@@ -1,4 +1,26 @@
 package com.example.popularmovieskotlin
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class MovieApi {
+    companion object {
+        private const val baseUrl = "https://api.themoviedb.org/3/"
+
+        fun createApi(): MovieApiService {
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+                .build()
+
+            val moviesApi = Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+            return moviesApi.create(MovieApiService::class.java)
+        }
+    }
 }
--- app/src/main/java/com/example/popularmovieskotlin/MovieApiService.kt
+++ app/src/main/java/com/example/popularmovieskotlin/MovieApiService.kt
@@ -1,4 +1,10 @@
 package com.example.popularmovieskotlin
 
-class MovieApiService {
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MovieApiService {
+    //TODO: WIP
+//    @GET("/discover/movie")
+//    suspend fun getMovies(@Query("")): MovieItem
 }
